bb87afcc20d4363ee4f11bbe36b06c071591704d
Trys <trysmudford@gmail.com>
2013-04-24 22:01:34 +0100

    Added station model

---
+++ js/geStation.js
@@ -0,0 +1,14 @@
+﻿$(function() {
+  "use strict";
+  var Station = Backbone.Model.extend({
+    defaults: function() {
+      return {
+        name: "empty station",
+        lat: 0,
+        lng: 0,
+        opened: 0,
+        closed: 0
+      };
+    }
+  });
+});
