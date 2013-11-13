dad41e76f7ae69a04bb1f615c79dbf358120a490
Trys <trysmudford@gmail.com>
2013-11-13 21:17:05 +0000

    Added station list sidebar and zoom to station feature

--- js/geStation.js
+++ js/geStation.js
@@ -1,6 +1,8 @@
 ﻿var geStation = (function () {
     "use strict";
     var isPlaying,
+    	chosenStation,
+        currentCenter = new google.maps.LatLng(54.226708, -2.790527),
         stationIndex = 0,
         stationCount = 0,
         markerIndex = 0,
@@ -14,11 +16,12 @@
             stationCount = geStation.Stations.length;
             geStation.Maps.initialise();
             $(".js-play").on("click", geStation.Animation.Toggle);
+            $("#stationlist").on("click", '.station_listing', geStation.Zoom.ZoomToStation);
         },
         Maps: {
             initialise: function () {
                 var mapOptions = {
-                    center: new google.maps.LatLng(54.226708, -2.790527),
+                    center: currentCenter,
                     zoom: 6,
                     mapTypeId: google.maps.MapTypeId.ROADMAP
                 };
@@ -47,6 +50,7 @@
                     }
                 });
                 geStation.Markers[stationIndex] = marker;
+                $('#bb-station-list').append('<li class="station_listing" id="station'+stationIndex+'">'+station.name+'</li>');
                 stationIndex++;
                 setTimeout(geStation.Maps.addMarker, 20);
             }
@@ -64,7 +68,6 @@
                 var addClass = isPlaying ? iconPause : iconPlay;
                 var removeClass = isPlaying ? iconPlay : iconPause;
                 $(".js-play i").addClass(addClass).removeClass(removeClass);
-                
             },
             ShowStation: function () {
                 if (geStation.Stations.length === markerIndex || !isPlaying) {
@@ -75,7 +78,18 @@
                 $('#js-current-year').text(geStation.Stations[markerIndex].opened);
                 markerIndex++;
             }
+        },
+        Zoom: {
+        	ZoomToStation: function () {
+        		chosenStation = $(this).attr('id').replace("station", "");
+                currentCenter = new google.maps.LatLng(geStation.Stations[chosenStation].lng, geStation.Stations[chosenStation].lat);
+                geStation.Map.panTo(currentCenter);
+                geStation.Map.setZoom(13);
+        	},
         }
     };
 }());
-$(document).ready(function () { geStation.Initialise(); });
+$(document).ready(function () { 		
+	geStation.Initialise();
+});
+
